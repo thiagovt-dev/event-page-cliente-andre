6120bb7f123a3bcb143a3ae62388f3756366b811
thiagovt-dev <thiagovt.dev@gmail.com>
2025-08-18 09:36:43 -0300

    chore: update Vite configuration for improved build process

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
@@ -13,5 +12,8 @@ export default defineConfig(({ mode }) => ({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    outDir: "build",
   },
 }));
